0f9de93d9552ae26e17a8d8e41d7a819381cfdfa
rodrigo <rodri.valdez@gmail.com>
2015-09-28 17:27:37 -0400

    settings database url

--- agenvida/settings.py
+++ agenvida/settings.py
@@ -104,12 +104,9 @@ WSGI_APPLICATION = 'agenvida.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
