2ef5a396cad240b9f7d55b9b1673277db89546bf
rodrigo <rodri.valdez@gmail.com>
2015-10-11 20:11:58 -0300

    cualquiera puede crear usuario

--- mainApp/serializers.py
+++ mainApp/serializers.py
@@ -31,7 +31,7 @@ class PropositoSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('first_name', 'last_name', 'email','username')
+        fields = ('password', 'first_name', 'last_name', 'email','username')
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
  
