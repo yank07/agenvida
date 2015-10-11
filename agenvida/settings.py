b71f546b7286c5726eccce469f8d278ff25970c8
rodrigo <rodri.valdez@gmail.com>
2015-10-11 20:02:27 -0300

    cualquiera puede crear usuario

--- agenvida/settings.py
+++ agenvida/settings.py
@@ -135,7 +135,7 @@ AUTHENTICATION_BACKENDS = (
 )
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
